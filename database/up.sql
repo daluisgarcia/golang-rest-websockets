8263bd570b9970acb4328ac8bc3595eeaed5240c
Daniel Luis <daluisgarcia@gmail.com>
2022-11-22 14:49:51 -0400

    SQL code to create users table

---
+++ database/up.sql
@@ -0,0 +1,10 @@
+DROP IF EXISTS TABLE `users`;
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+)
