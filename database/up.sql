ddd57cf4ae21e501d03b6bad6978d89f54993552
Daniel Luis <daluisgarcia@gmail.com>
2022-11-22 15:16:21 -0400

    SQL code updated and execution errors fixed

--- database/up.sql
+++ database/up.sql
@@ -1,10 +1,8 @@
-DROP IF EXISTS TABLE `users`;
+DROP TABLE IF EXISTS "users";
 
-CREATE TABLE `users` (
-  `id` varchar(36) NOT NULL AUTO_INCREMENT,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-)
+CREATE TABLE "users" (
+  id varchar(36) NOT NULL PRIMARY KEY,
+  email varchar(255) NOT NULL,
+  password varchar(255) NOT NULL,
+  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
